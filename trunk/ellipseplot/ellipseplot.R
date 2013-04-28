853e83b16299ec1fa78e3217a4a54476b9ad5505
cowares@gmail.com <cowares@gmail.com@eb3de0eb-aa91-7093-4a15-7d322daef34f>
2013-04-28 10:32:59 +0000

    update ellipseplot split test functions

--- trunk/ellipseplot/ellipseplot.R
+++ trunk/ellipseplot/ellipseplot.R
@@ -185,36 +185,3 @@ calc.abox <- function(stat) {
          })
 }
 
-
-# test functions
-
-testdata.onebox <- data.frame(low=1:0,center=3:4,high=6:5,row.names=c('x','y'))
-
-test.anellipse <- function(x=testdata.onebox, verbose=F) {
-  plot(t(x['x',]), t(x['y',]))
-  anellipse(x, verbose, col='red', border='blue', lty='dotted')
-}
-
-test.ninenum <- function() {
-  print(ninenum(1:9))
-  print(ninenum(1:999))
-  print(ninenum(1:1000))
-  print(ninenum(c(9:1,NA)))
-  print(ninenum(rep(NA,9)))
-  invisible(T)
-}
-
-test.manyellipses <- function(...) {
-  SUMMARY=ninenum
-  stats <- list(
-                data.frame(x=SUMMARY(rnorm(10)), y=SUMMARY(rnorm(10))),
-                data.frame(x=SUMMARY(rnorm(10,1)), y=SUMMARY(rnorm(10))),
-                data.frame(x=SUMMARY(rnorm(10,2)), y=SUMMARY(rnorm(10,1))),
-                data.frame(x=SUMMARY(rnorm(10,3)), y=SUMMARY(rnorm(10,1))),
-                data.frame(x=SUMMARY(rnorm(10,4)), y=SUMMARY(rnorm(10,4)))
-                )
-  many.ellipses(stats, ...) 
-}
-
-#test.anellipse(verbose=T)
-
