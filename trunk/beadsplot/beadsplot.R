0f93fe5994f6508b8cfc003ca4a2a71464930ec9
cowares@gmail.com <cowares@gmail.com@eb3de0eb-aa91-7093-4a15-7d322daef34f>
2013-10-24 12:15:59 +0000

    split util_r from beadsplot and boxplotdou

--- trunk/beadsplot/beadsplot.R
+++ trunk/beadsplot/beadsplot.R
@@ -335,30 +335,6 @@ make.scale <- function(...) {
   scale
 }
 
-only.graphic.pars <- function(pars, what='plot.default') {
-  if(length(pars) == 0) return(list())
-
-  gnames <- names(par(no.readonly=T))
-  pnames <- names(formals(args(what)))
-  gpnames <- unique(c(pnames[pnames != '...'], gnames))
-
-  selectgname <- pars[gpnames]
-  to.rm.na <- names(selectgname)
-
-  if(is.null(to.rm.na)) list() else 
-    selectgname[!is.na(to.rm.na)]
-}
-
-do.call.with.par <- function(what, args, dots.win=FALSE, ...) {
-  pars <- 
-    if(dots.win) {
-      modifyList(only.graphic.pars(args, what), list(...))
-    } else {
-      modifyList(list(...), only.graphic.pars(args, what))
-    }
-  do.call(what, pars)
-}
-
 plt.init <- function(lim, ...) {
   axt <- rep('s', 2)
   names(axt) <- dimnames(lim)[[2]]
@@ -479,49 +455,6 @@ plt.diamond <- function(pt, col, border, lwd=1, lwd.center=lwd,
           border=if(is.null(lwd)) NA else border)
   if(!is.null(lwd.center))
     lines(x=pt[c(1,3),1], y=pt[c(1,3),2], col=border, lwd=lwd.center)
-}
-
-make.shadings <- function(n, density=NA, angle=NA, verbose=FALSE) {
-  shadings <- list(density=NULL, angle=NULL)
-
-  if(is.na(density) && is.na(angle)) return(shadings)
-
-  label <- c('density', 'angle')
-  start <- c(12, 10)
-  end <- c(36, 160)
-  shuffling <- c(TRUE, FALSE)
-  par <- list(density, angle)
-
-  for(i in 1:2) {
-    x <- par[[i]]
-
-    if(length(x) >= n) {
-      shadings[[i]] <- x[1L:n]
-    } else {
-      rx <- if(is.numeric(x)) rep(x, 2)[1:2] else rep(NA, 2)
-      if(is.na(rx[1])) rx[1] <- start[i]
-      if(is.na(rx[2])) rx[2] <- end[i]
-
-      sx <- seq(from=rx[1], to=rx[2], length.out=n)
-
-      if(shuffling[i]) {
-        # shuffle by halves
-        # 1, k, 2, k+1, 3, k+2,,, n
-        shuffle <- seq(from=1, to=n, by=2)
-        shuffle <- c(shuffle, shuffle + 1)[1L:n]
-      } else {
-        shuffle <- 1L:n
-      }
-      shadings[[i]] <- sx[order(shuffle)]
-    }
-  }
-
-  if(verbose) {
-    cat('# shading enabled\n')
-    print(shadings)
-  }
-
-  shadings
 }
 
 plt.beads <- function(stats, scale, 
